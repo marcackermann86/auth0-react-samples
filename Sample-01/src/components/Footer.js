4ff83ba54c718867c1414d5ad891173ee29dcd03
marcackermann86 <99517033+marcackermann86@users.noreply.github.com>
2025-10-28 16:41:15 -0400

    Update Footer.js

--- Sample-01/src/components/Footer.js
+++ Sample-01/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="bg-light p-3 text-center">
+  <footer className="bg-dark p-3 text-center">
     <div className="logo" />
     <p>
       Sample project provided by <a href="https://auth0.com">Auth0</a>
